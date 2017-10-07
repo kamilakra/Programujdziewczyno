82e8bb034edcd0e7743911010590f578227835b8
kamilakra <32577264+kamilakra@users.noreply.github.com>
2017-10-07 15:56:50 +0200

    show dropdown on hover

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict'
+  document.addEventListener('DOMContentLoaded', function() {
+      console.log('działa');
+      
+      //Zadanie 1
+      var forDropdown = document.querySelector('.for-dropdown');
+      console.log(forDropdown);
+      var dropdown = document.querySelector('.dropdown');
+      console.log(dropdown);
+      
+      forDropdown.addEventListener('mouseover', function(){
+        dropdown.style.display = 'block';
+      });
+           
+      forDropdown.addEventListener('mouseout', function(){
+        dropdown.style.display = 'none'; 
+      }); 
+    });
